6470e1a8d1fd2b23fc27d2e964aa6f0f97caf0c3
ShanuJo <105334806+ShanuJo@users.noreply.github.com>
2024-08-05 14:31:34 +0200

    Initial commit

---
+++ src/components/UserFinder.js
@@ -0,0 +1,73 @@
+import { Fragment, useState, useEffect } from "react";
+
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+import { Component } from "react/cjs/react.production.min";
+import UsersContext from "../store/users-context";
+
+// const DUMMY_USERS = [
+//   { id: "u1", name: "Max" },
+//   { id: "u2", name: "Manuel" },
+//   { id: "u3", name: "Julie" },
+// ];
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+    // this.searchChangeHandler = this.searchChangeHandler.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    // this.setState((curState) => {
+    //   return {curState.searchTerm:event.target.value};
+    // });
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+// };
+
+export default UserFinder;
